873d99ae0fb48f48d90e7d0f0d20466de73af809
rahul05-code <kanzariyarahul31@gmail.com>
2025-06-27 11:12:36 +0530

    Add project files.

---
+++ 24SOECE13010_RK/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Read();
+        }
+    }
+}
