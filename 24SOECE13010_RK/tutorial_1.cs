018dd9b911c7db367411bd926d1acac3d05650cd
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-04 20:21:46 +0530

    t5

--- 24SOECE13010_RK/tutorial_1.cs
+++ 24SOECE13010_RK/tutorial_1.cs
@@ -1,32 +1,30 @@
-﻿using System;
+﻿using System; //system name space provide input/output
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _24SOECE13010_RK
+namespace _24SOECE13010_RK //name space for program
 {
-    internal class tutorial_1
+    internal class tutorial_1 //class name
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)//main method
         {
-            int x;
+            int n, fact=1; //n variable declare and fact initializes
             
-            Console.Write("Enter the number:");
-            string str = Console.ReadLine();
+            Console.Write("Enter the number:"); //prompts for user enter 
+            string str = Console.ReadLine(); // reads input
 
-            x = Convert.ToInt32(str);
+            n = Convert.ToInt32(str); //converts string to intrger
 
-            if(x % 2 == 0)
+            for (int i = 1; i <= n; i++) //loops from 1 to 'n'
             {
-                Console.WriteLine("Number is Even.");
-            }
-            else
-            {
-                Console.WriteLine("The Number is Odd.");
+                fact = fact * i; //Calculates factorial by multiplying 'fact' with 'i' in each iteration
             }
 
-            Console.ReadLine(); 
+            Console.WriteLine("Factorial : {0}",fact);//display calculated factorial
+
+            Console.Read(); // waits for user to press a key before closing the console window
 
         }
     }
