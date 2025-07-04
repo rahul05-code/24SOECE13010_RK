1fe3301ddaaee0a46ef2fb0c021f798e819f5a92
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-04 11:31:52 +0530

    tutorial_1

---
+++ 24SOECE13010_RK/tutorial_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class tutorial_1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Rahul Kanzariya");
+            Console.WriteLine("DOB: 5/10/2005");
+            Console.WriteLine("Address: vajepar street 3 ");
+            Console.WriteLine("City: Morbi");
+            Console.WriteLine("Pin Code: 363641");
+            Console.WriteLine("State: Gujarat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email:rkanzariya861@rku.ac.in");
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
