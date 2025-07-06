97096fb45af0f53da002d2b7bec9af638ebd5894
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-06 15:29:56 +0530

    t9

---
+++ 24SOECE13010_RK/q9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q9
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Text :");
+            string input = Console.ReadLine();  
+
+            string name = " ";
+
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                {
+                    name += char.ToLower(c);
+                }
+                else if (char.IsLower(c))
+                {
+                    name += char.ToUpper(c);
+                }
+                else
+                {
+                    name += c; // Keep non-alphabetic characters unchanged
+                }
+            }
+
+            Console.WriteLine("Converted Text: " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
