1b3655e1f3ee234b32491488579fd172629c83e5
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-06 15:45:57 +0530

    q10

---
+++ 24SOECE13010_RK/q10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q10
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Mobile Number: ");
+            string m = Console.ReadLine();
+
+            if (m.Length > 5)
+            {
+                string firstPart = m.Substring(0, m.Length - 5);
+                string lastPart = new string('X',5);
+                string maskedNumber = firstPart + lastPart;
+                Console.WriteLine("Masked Mobile Number: " + maskedNumber);
+            }
+            else
+            {
+                Console.WriteLine("Mobile number is too short to mask.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 24SOECE13010_RK/q11.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q11
+    {
+
+    }
+}
