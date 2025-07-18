3b8cce5bae7984117446a38ba6063f0e48afbcce
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-18 10:17:36 +0530

    test

---
+++ 24SOECE13010_RK/class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class @class
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //a.Max();
+            Console.WriteLine("Maximun element is:"+a.Max());
+        }
+    }
+}
