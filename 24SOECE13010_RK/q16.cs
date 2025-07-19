302db5d1257d2d883d6f8fc79cb1ba39e78740e7
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 17:56:25 +0530

    q17

---
+++ 24SOECE13010_RK/q16.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q16
+    {
+        public static void Main(string[] args)
+        {
+            int n = 3;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 24SOECE13010_RK/q17.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q17
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
