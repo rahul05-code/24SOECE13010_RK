f5c479db55ede01cbd03da9ae8864a8b80225233
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 16:46:29 +0530

    q14

---
+++ 24SOECE13010_RK/q14.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q14
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+
+            string[] words = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+            foreach (char digit in number)
+            {
+                int d = digit - '0';
+                Console.Write(words[d] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
