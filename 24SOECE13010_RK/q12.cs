c1040492c4cd4163036ab95693672ca3d204965d
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 15:51:28 +0530

    tutorial q12

---
+++ 24SOECE13010_RK/q12.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q12
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("NAME : "+name);
+
+            Console.ReadLine();
+        }
+    }
+}
