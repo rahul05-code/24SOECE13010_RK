8bc5c3dca68f66fb6b2909a8741eae24d3b42cb5
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 17:13:47 +0530

    q15

---
+++ 24SOECE13010_RK/q15.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q15
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int temp = n;
+            while (temp > 0)
+            {
+                int d = temp % 10;
+                a += d * d * d;
+                temp /= 10;
+            }
+
+            if(a == n)
+            {
+                Console.WriteLine(n+" Is Armstrong number");
+            }
+            else
+            {
+                Console.WriteLine(n+" Not an Armstrong number");
+            }   
+            Console.ReadLine();
+        }
+    }
+}
