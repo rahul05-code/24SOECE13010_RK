9b09742d1bf12cad9445b64a6c4ff04a5566d14f
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-22 17:34:58 +0530

    q19

---
+++ 24SOECE13010_RK/q19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q19
+    {
+        public static void Main(string[] args)
+        {
+            int num = 4;
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1;j <= num - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int k = 1;k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
