237c881350b9fc8be73d578348d99400706a20d0
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-22 17:08:51 +0530

    q18

--- 24SOECE13010_RK/q18.cs
+++ 24SOECE13010_RK/q18.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace _24SOECE13010_RK
 {
-    internal class q17
+    internal class q18
     {
         public static void Main(string[] args)
         {
+            int num = 1;
             for (int i = 1; i <= 4; i++)
             {
-                //int k = i;
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write(j);
+                    Console.Write(num);
+                    num++;
                 }
                 Console.WriteLine();
             }
