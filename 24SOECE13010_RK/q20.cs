a929307b007db83497b7ec5a5d0a5633d74fc08a
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-22 17:39:39 +0530

    q20 easy

---
+++ 24SOECE13010_RK/q20.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class q20
+    {
+        public static void Main(string[] args)
+        {
+            int num = 4;
+            int n = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(n+" ");
+                    n++;
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
