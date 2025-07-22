1709e6b8a37bb324a3b072eee953073abce29d0d
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-22 16:54:05 +0530

    q17

--- 24SOECE13010_RK/q17.cs
+++ 24SOECE13010_RK/q17.cs
@@ -10,9 +10,10 @@ namespace _24SOECE13010_RK
     {
         public static void Main(string[] args)
         {
-            for (int i = 1; i <= 5; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                for (int j = 1; j <= 5; j++)
+                int k = i;
+                for (int j = 1; j <= k; j++)
                 {
                     Console.Write(j);
                 }
