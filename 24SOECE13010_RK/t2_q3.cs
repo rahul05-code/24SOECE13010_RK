1ab5bea8c69439ab60abcb2235710a77992f17d7
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-09 10:11:32 +0530

    tutorial 2 , Q - 3

---
+++ 24SOECE13010_RK/t2_q3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q3
+    {
+        static void Main(string[] args)
+
+        {
+
+            string firstName = "John";
+
+            string lastName = "Doe";
+
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+
+
+
+            Console.WriteLine("Please enter a new first name:");
+
+            firstName = Convert.ToString(Console.ReadLine());
+
+
+
+            Console.WriteLine("New name: " + firstName + " " + lastName);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
