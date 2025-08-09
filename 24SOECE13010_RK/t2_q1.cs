6ac5599db740bd9767c7da40661a1cbdb9d2149d
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-09 09:36:50 +0530

    tutorial 2 , Q - 1

---
+++ 24SOECE13010_RK/t2_q1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q1
+    {
+        static void Main(string[] args)
+
+        {
+
+            /* local variable definition */
+
+            int a = 10;
+
+
+
+            /* check the boolean condition using if statement */
+
+            if (a < 20)
+
+            {
+
+                /* if condition is true then print the following */
+
+                Console.WriteLine("a is less than 20");
+
+            }
+
+            Console.WriteLine("value of a is : {0}", a);
+
+            Console.ReadLine();
+
+        }
+    }
+}
