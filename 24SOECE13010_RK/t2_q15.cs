b8028153eff7ec2d5d982247ad50d3819fb10fba
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 15:25:38 +0530

    t2 q15 complete tutorial 2

---
+++ 24SOECE13010_RK/t2_q15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q15
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("INPUT: ");
+
+            string s = Console.ReadLine();
+            
+            Console.WriteLine("OUTPUT:"+s);
+        }
+    }
+}
