4ed150db844ed3192b05c7cc44afdfd8de7582d6
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 14:04:32 +0530

    t2 q9

---
+++ 24SOECE13010_RK/t2_q9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q9
+    {
+        public static void Main(string[] args)
+        {
+     
+            int[] arr = { 1, 2, 3, 2, 1, 4 };
+
+            int xor = 0;
+
+            
+            foreach (int num in arr)
+            {
+                xor ^= num;
+            }
+
+            
+            int rightmostSetBit = xor & -xor;
+
+            int num1 = 0, num2 = 0;
+
+            
+            foreach (int num in arr)
+            {
+                if ((num & rightmostSetBit) != 0)
+                    num1 ^= num;
+                else
+                    num2 ^= num;
+            }
+
+            Console.WriteLine("The two unique numbers are: {0} {1}", num1, num2);
+        }
+    }
+}
