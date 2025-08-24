ad5ad81491734610a41b4bc47b97dedde542e16d
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 15:15:59 +0530

    t2 q14

---
+++ 24SOECE13010_RK/t2_q14.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q14
+    {
+        public static void Main(string[]args)
+        {
+            Console.Write("Input the marks obtained in Maths: ");
+            int maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the marks obtained in Physics: ");
+            int physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the marks obtained in Chemistry: ");
+            int chemistry = Convert.ToInt32(Console.ReadLine());
+
+            int total = maths + physics + chemistry;
+            int totalMP = maths + physics;
+
+            if (maths >= 65 && physics >= 55 && chemistry >= 50 &&
+                (total >= 180 || totalMP >= 140))
+            {
+                Console.WriteLine("The candidate is eligible for admission.");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is not eligible for admission.");
+            }
+        }
+    }
+}
