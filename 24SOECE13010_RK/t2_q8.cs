e08d7a3eae12fefa0882ead01b7676a273847909
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 13:42:52 +0530

    t2 q8

---
+++ 24SOECE13010_RK/t2_q8.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    class NumberManipulator2
+    {
+        public int getValues(out int x, out int y, out int z)
+        {
+            Console.WriteLine("Enter the first value: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third value: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            int sum = x + y + z; // corrected sum calculation
+            return sum;
+        }
+    }
+
+    class t2_q8
+    {
+        static void Main(string[] args)
+        {
+            NumberManipulator2 n = new NumberManipulator2();
+
+            /* local variable definition */
+            int a, b, c, sum;
+
+            /* calling a function to get the values */
+            sum = n.getValues(out a, out b, out c);
+
+            Console.WriteLine("After method call, value of a : {0}", a);
+            Console.WriteLine("After method call, value of b : {0}", b);
+            Console.WriteLine("After method call, value of c : {0}", c);
+            Console.WriteLine("Sum : {0}", sum);
+        }
+    }
+    
+}
