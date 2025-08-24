e7e708724af6aca6c1d64d4f2470b7c9dee96cce
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 14:32:06 +0530

    t2 q11

---
+++ 24SOECE13010_RK/t2_q11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q11
+    {
+        public static void Main(string[]args)
+        {
+            Console.Write("Enter number of elements (N): ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[N];
+            int sum = 0;
+
+            Console.WriteLine("Enter {0} elements:", N);
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Element {0}: ", i + 1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i]; 
+            }
+
+            Console.WriteLine("Sum of array elements = " + sum);
+        }
+    }
+}
