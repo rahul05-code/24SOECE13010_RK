664f1143515f177f82bf3717b864561c2b878bd8
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 13:26:00 +0530

    t2 q6

---
+++ 24SOECE13010_RK/t2_q6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q6
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You entered the following {0} command line arguments:",
+               args.Length);
+
+            
+            for (int i = 0; i < args.Length; i++)      
+            {                                            
+                Console.WriteLine(args[i]);               
+            }                                            
+        }
+    }
+}
