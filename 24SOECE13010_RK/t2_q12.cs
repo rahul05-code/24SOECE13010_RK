cc3d8274e4977b0ee9db2e0e9d24db255b7cee60
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-24 14:39:48 +0530

    t2 q12

---
+++ 24SOECE13010_RK/t2_q12.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_RK
+{
+    internal class t2_q12
+    {
+        public static void Main(string[]args)
+        {
+            Console.Write("Enter number of elements (N): ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[N];
+
+            Console.WriteLine("Enter {0} elements:", N);
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Element {0}: ", i + 1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Enter element to search: ");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            int found = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] == X)
+                {
+                    found = 1;
+                    break;
+                }
+            }
+
+            Console.WriteLine(found);
+        }
+    }
+}
